b2f6b36801a8f6a8b6578eed1b82353a58a086ed
TheCrether <denisimeri6@gmail.com>
2020-01-03 22:26:16 +0100

    fixed eslintrc again

--- src/preload.js
+++ src/preload.js
@@ -1,9 +1,11 @@
-const _setImmediate = setImmediate
-const _clearImmediate = clearImmediate
+const _setImmediate = setImmediate;
+const _clearImmediate = clearImmediate;
 const _process = process;
+const _require = require;
 process.once('loaded', () => {
-  global.setImmediate = _setImmediate
-  global.clearImmediate = _clearImmediate
+  global.setImmediate = _setImmediate;
+  global.clearImmediate = _clearImmediate;
   global.process = _process;
   window.process = _process;
+  window.require = _require;
 });
